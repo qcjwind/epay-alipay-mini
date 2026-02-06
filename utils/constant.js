a53a66d60cd826a920395bab60f06b0a866cb4d3
baoguosen <2563209120@qq.com>
2026-02-06 09:41:35 +0800

    feat: 域名变更

--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,11 @@
 // export const BASE_URL = "http://10.113.1.31:8080";
 // export const BASE_URL = "https://adc.natapp1.cc"
-export const BASE_URL = "https://zongmu.fun"
+// export const BASE_URL = "https://zongmu.fun"
+
+// DEV
+// export const BASE_URL = "https://xaplus-epay-topup-mp-dev.rebellapp.com"
+
+// UAT
+export const BASE_URL = "https://xaplus-epay-topup-mp-uat.rebellapp.com"
 
 export const RB_TOKEN = "RB_TOKEN"
